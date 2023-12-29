f754f6e4befb25c6c53f7f5a0502a49945dee84a
ZacharyLawPep <zacharylaw@peplink.com>
2023-12-29 16:11:54 +0800

    Remove cloud block

---
+++ aws.tf
@@ -0,0 +1,36 @@
+terraform {
+  cloud {
+    organization = "zac-aws"
+    workspaces {name = "learn-terraform-cloud"}
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.31.0"
+    }
+  }
+  required_version = "~> 1.2"
+}
+variable "region"        {default     = "us-west-1"}
+variable "instance_type" {default     = "t2.nano"}
+variable "instance_name" {default     = "[Zachary] Terraform"}
+provider "aws" {  region = var.region}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+resource "aws_instance" "ubuntu" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  tags          = {Name = var.instance_name}
+}
+output "instance_ami" {value = aws_instance.ubuntu.ami}
+output "instance_arn" {value = aws_instance.ubuntu.arn}
