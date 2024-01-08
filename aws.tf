3366f0300c7221046b819e0aac3f230f11328639
ZacharyLawPep <zacharylaw@peplink.com>
2024-01-08 18:40:50 +0800

    simpler?

--- aws.tf
+++ aws.tf
@@ -1,4 +1,3 @@
-
 terraform {
   cloud {
     organization = "zac-aws"
@@ -10,93 +9,30 @@ terraform {
       version = "~> 5.31.0"
     }
   }
-  required_version = "~> 1.2"
+  required_version = "~> 1.6.6"
 }
-variable "instance_type" {default = "t2.nano"}
-variable "instance_name" {default = "[Zachary] Terraform FusionHub"}
 provider "aws"{
   region  = "us-east-1" //N. Virgina
-  #shared_credentials_files = ["/Users/tf_user/.aws/creds"]
-  #profile                  = "default"
 }
 resource "aws_vpc" "my_vpc" {
+  enable_dns_hostnames = true
   cidr_block = "10.0.0.0/16"
-    tags = {Name = "[Zachary] Terraform 10.0/16"}
-
+  tags = {Name = "[Zachary] Terraform 10.0/16"}
 }
-resource "aws_security_group" "my_sg" {
-  name        = "[Zachary] Terraform SG"
-  vpc_id      = aws_vpc.my_vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-    ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-    ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1" // any protocol
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1" // any protocol
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_subnet" "my_subnet" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.0.0/24"
   tags       = { Name = "[Zachary] Terraform Subnet" }
 }
-
+resource "aws_network_interface" "aws_network_interface-1fd5b0a9" {
+  subnet_id = aws_subnet.my_subnet.id
+  private_ips = ["172.16.10.100",]
+}
 resource "aws_instance" "ubuntu" {
-  tags                   = { Name = "[Zachary] Terraform fusionhub" }
-  ami                    = "ami-0098c6e7b556afbc2"
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.my_sg.id]
-  subnet_id              = aws_subnet.my_subnet.id
-  associate_public_ip_address = true
-  user_data = <<-EOF
-            sudo shutdown +30 
-            #30min
-            EOF
-
-  lifecycle {
-  create_before_destroy = true
-  ignore_changes = [tags, instance_type, key_name]
-  prevent_destroy = false
-}
-}
-
-resource "aws_instance" "example_instance" {
   ami                    = "ami-0c7217cdde317cfec"
   instance_type          = "t2.micro"
   tags                   = { Name = "[Zachary] Terraform user_data" }
-  vpc_security_group_ids = [aws_security_group.my_sg.id]
+  #vpc_security_group_ids = [aws_security_group.my_sg.id]
   subnet_id              = aws_subnet.my_subnet.id
   associate_public_ip_address = true
   user_data = <<-EOF
@@ -116,14 +52,8 @@ resource "aws_instance" "example_instance" {
 }
 resource "aws_ec2_instance_state" "ubuntu" {
   instance_id = aws_instance.ubuntu.id
-  state       = "stopped"#stopped
+  state       = "running"#stopped running
 }
-resource "aws_ec2_instance_state" "example_instance" {
-  instance_id = aws_instance.ubuntu.id
-  state       = "stopped"#stopped
-}
-output "instance_ami" {value = aws_instance.ubuntu.ami}
-output "instance_arn" {value = aws_instance.ubuntu.arn}
 output "instance_private_ip" {value = aws_instance.ubuntu.private_ip}
 output "instance_public_ip" {value = aws_instance.ubuntu.public_ip}
 
--- readme.md
+++ readme.md
@@ -5,3 +5,18 @@ Maybe change name and shrink
 Auto apply for cli and git
 
 How to stop it
+
+$ rover -workingDir "example/eks-cluster" -tfPath "/Users/dos/terraform"
+
+
+C:\Users\zacharylaw\Desktop\HTML\Terra\aws\learn-terraform-cloud\rover\rover_v0.3.2.exe -workingDir "C:\Users\zacharylaw\Desktop\HTML\Terra" -tfPath C:\Users\zacharylaw\Desktop\HTML\Terra\aws\learn-terraform-cloud
+C:\Users\zacharylaw\Desktop\HTML\Terra\aws\learn-terraform-cloud
+2024/01/08 13:29:27 Unable to parse Plan: Unable to initialize Terraform Plan: exec: "/usr/local/bin/terraform": file does not exist
+
+C:\Users\zacharylaw\Desktop\HTML\Terra
+
+
+C:\Users\zacharylaw\Desktop\HTML\Terra\aws\learn-terraform-cloud\rover\rover.exe . -tfPath "C:\Users\zacharylaw\Desktop\HTML\Terra\terraform.exe" -tfcOrg=hashicorp-training -tfcWorkspace=tf-random -tfcNewRun=true -tfConfigExists=false
+
+
+-tfcOrg=hashicorp-training -tfcWorkspace=tf-random -tfcNewRun=true -tfConfigExists=false
