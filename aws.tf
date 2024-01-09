9d3f86a5f7971f1a4466489c0c37dcc372b0767d
ZacharyLawPep <zacharylaw@peplink.com>
2024-01-09 12:23:23 +0800

    new apache?

--- aws.tf
+++ aws.tf
@@ -14,33 +14,46 @@ terraform {
 provider "aws"{
   region  = "us-east-1" //N. Virgina
 }
+resource "aws_security_group" "web_server" {
+  name = var.server_name
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_vpc" "my_vpc" {
   enable_dns_hostnames = true
   cidr_block = "10.100.100.0/16"
   tags = {Name = "[Zachary] Terraform 10.100/16"}
 }
-resource "aws_subnet" "my_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.100.100.0/24"
-  tags       = { Name = "[Zachary] Terraform Subnet" }
-}
-resource "aws_network_interface" "aws_network_interface-1fd5b0a9" {
-  subnet_id = aws_subnet.my_subnet.id
-  private_ips = ["10.100.100.100",]
-}
 resource "aws_instance" "ubuntu" {
   ami                    = "ami-0c7217cdde317cfec"
   instance_type          = "t2.micro"
   tags                   = { Name = "[Zachary] Terraform user_data" }
-  #vpc_security_group_ids = [aws_security_group.my_sg.id]
-  subnet_id              = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
   associate_public_ip_address = true
   user_data = <<-EOF
     #!/bin/bash
-    yum update -y
-    service httpd start
-    chkconfig httpd on
-
+    sudo apt update -y
+    sudo apt install apache2 -y
+    
     sudo shutdown +30 
     #30min
   EOF
@@ -48,7 +61,11 @@ resource "aws_instance" "ubuntu" {
   create_before_destroy = true
   ignore_changes = [tags, instance_type, key_name]
   prevent_destroy = false
+  }
 }
+resource "aws_key_pair" "deployer" {
+  key_name   = "ssh_key_name"
+  public_key = file("C:/Users/zacharylaw/.ssh/id_rsa.pub")
 }
 resource "aws_ec2_instance_state" "ubuntu" {
   instance_id = aws_instance.ubuntu.id
